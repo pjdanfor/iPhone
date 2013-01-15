4c1523574362e01863100084041777fe562e7649
Patrick Danford <pat.danford@gmail.com>
2013-01-15 01:25:35 -0500

    add projectiles and collision detection… BOOM!

--- TutorialGame/TutorialGame/HelloWorldLayer.m
+++ TutorialGame/TutorialGame/HelloWorldLayer.m
@@ -13,6 +13,9 @@
 // HelloWorldLayer implementation
 @implementation HelloWorldLayer
 
+NSMutableArray * _monsters;
+NSMutableArray * _projectiles;
+
 +(CCScene *) scene
 {
 	// 'scene' is an autorelease object.
@@ -34,6 +37,8 @@
 
 - (void) addMonster {
     CCSprite *monster = [CCSprite spriteWithFile:@"monster.png"];
+    monster.tag = 1;
+    [_monsters addObject:monster];
     
     // Determine where to spawn the monster along the Y axis
     CGSize winSize = [CCDirector sharedDirector].winSize;
@@ -58,8 +63,82 @@
                                                position:ccp(-monster.contentSize.width/2, actualY)];
     CCCallBlockN *actionMoveDone = [CCCallBlockN actionWithBlock: ^(CCNode *node) {
         [node removeFromParentAndCleanup:YES]; 
+        [_monsters removeObject:node];
     }];
     [monster runAction:[CCSequence actions:actionMove, actionMoveDone, nil]];
+}
+
+- (void) ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    // Choose one of the touches to work with
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [self convertTouchToNodeSpace:touch];
+    
+    // Initial point of projectile
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    CCSprite *projectile = [CCSprite spriteWithFile:@"projectile.png"];
+    projectile.position = ccp(20, winSize.height/2);
+    projectile.tag = 2;
+    [_projectiles addObject:projectile];
+    
+    // Determine offset of location to projectile
+    CGPoint offset = ccpSub(location, projectile.position);
+    
+    if (offset.x <= 0)
+        return;
+    
+    // Add projectile
+    [self addChild:projectile];
+    
+    int realX = winSize.width + (projectile.contentSize.width/2);
+    float ratio = (float) offset.y / (float) offset.x;
+    int realY = (realX * ratio) + projectile.position.y;
+    CGPoint realDest = ccp(realX, realY);
+    
+    // Determine the length of how far you're shooting
+    int offRealX = realX - projectile.position.x;
+    int offRealY = realY - projectile.position.y;
+    float length = sqrtf((offRealX * offRealX) + (offRealY * offRealY));
+    float velocity = 480/1;
+    float realMoveDuration = length/velocity;
+    
+    // Move projectile to actual point
+    [projectile runAction:
+     [CCSequence actions:
+      [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
+      [CCCallBlockN actionWithBlock:^(CCNode *node) {
+         [node removeFromParentAndCleanup:YES];
+         [_projectiles removeObject:node];
+     }],
+      nil]];
+}
+
+- (void) update:(ccTime)dt {
+    NSMutableArray *projectilesToDelete = [[NSMutableArray alloc] init];
+    for (CCSprite *projectile in _projectiles) {
+        NSMutableArray *monstersToDelete = [[NSMutableArray alloc] init];
+        for (CCSprite *monster in _monsters) {
+            if (CGRectIntersectsRect(projectile.boundingBox, monster.boundingBox)) {
+                [monstersToDelete addObject:monster];
+            }
+        }
+        
+        for (CCSprite *monster in monstersToDelete) {
+            [_monsters removeObject:monster];
+            [self removeChild:monster cleanup:YES];
+        }
+        
+        if (monstersToDelete.count > 0) {
+            [projectilesToDelete addObject:projectile];
+        }
+        [monstersToDelete release];
+    }
+    
+    for (CCSprite *projectile in projectilesToDelete) {
+        [_projectiles removeObject:projectile];
+        [self removeChild:projectile cleanup:YES];
+    }
+    [projectilesToDelete release];
 }
 
 // on "init" you need to initialize your instance
@@ -70,8 +149,13 @@
         CCSprite *player = [CCSprite spriteWithFile:@"player.png"];
         player.position = ccp(player.contentSize.width/2, winSize.height/2);
         [self addChild:player];
+        
+        _monsters = [[NSMutableArray alloc] init];
+        _projectiles = [[NSMutableArray alloc] init];
     }
+    [self schedule:@selector(update:)];
     [self schedule:@selector(gameLogic:) interval:1.0];
+    [self setIsTouchEnabled:YES];
     return self;
 }
 
@@ -84,5 +168,10 @@
 	
 	// don't forget to call "super dealloc"
 	[super dealloc];
+    
+    [_monsters release];
+    _monsters = nil;
+    [_projectiles release];
+    _projectiles = nil;
 }
 @end
